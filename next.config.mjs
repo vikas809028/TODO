6c33b3d57781b5c10b05fb690c86c168577300b3
vikas809028 <vikastiwari809028@gmail.com>
2025-02-11 15:34:35 +0530

    bug fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    output: "standalone",
+  };
+  
+  module.exports = nextConfig;
+  
